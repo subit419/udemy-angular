d8552dcb498af7a8e9c018887e3b2cbdd01435bd
Mark Subit <marksubit@gmail.com>
2024-03-02 13:55:10 -0500

    empty

--- udemy-course-L1/src/app/servers/servers.component.ts
+++ udemy-course-L1/src/app/servers/servers.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server>',
-  styleUrl: './servers.component.scss'
+  templateUrl: './servers.component.html',
+  styleUrl: './servers.component.scss',
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
+    this.servers.push(this.serverName);
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
